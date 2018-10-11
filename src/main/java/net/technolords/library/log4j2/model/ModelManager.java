80867a85a74765f31327034a50f8e8061203475f
SrideviJava <jsridevi@gmail.com>
2018-10-11 16:29:45 +0200

    Issue#1 - Added Unit Test for Extraction of Key and Value from Layout Pattern

--- src/main/java/net/technolords/library/log4j2/model/ModelManager.java
+++ src/main/java/net/technolords/library/log4j2/model/ModelManager.java
@@ -1,11 +1,16 @@
 package net.technolords.library.log4j2.model;
 
+import org.slf4j.LoggerFactory;
+
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ModelManager {
+    private final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     /*
         This class has the following responsobilies:
@@ -26,10 +31,17 @@ public class ModelManager {
     public Map<String, String> convertLayoutPatternToMap(String layoutPattern) {
         String[] logEvents = layoutPattern.split("\\s+");
         HashMap<String, String> hmap = new HashMap<>();
+        String eventName = null;
 
 
         for (String event : logEvents) {
             String conversionString, key = null;
+            Pattern p1 = Pattern.compile("\\{\\w*\\}");
+            Matcher m1 = p1.matcher(event);
+            if (m1.find()) {
+                eventName = m1.group();
+            }
+
             event = event.replaceAll("\\{.*\\}", "");
 
             Pattern p2 = Pattern.compile("(\\[%.*]|%.*)");
@@ -43,7 +55,7 @@ public class ModelManager {
                 event = conversionString;
 
                 conversionString = conversionString.replaceAll("%","");
-                key = conversionString;
+                key = conversionString+eventName;
             }
             hmap.put(key,event);
         }
--- src/test/main/java/net.technolords.library.log4j2/ModelManagerTest.java
+++ src/test/main/java/net.technolords.library.log4j2/ModelManagerTest.java
@@ -1,6 +1,9 @@
 package net.technolords.library.log4j2;
 
 import net.technolords.library.log4j2.model.ModelManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.Iterator;
@@ -8,12 +11,21 @@ import java.util.Map;
 import java.util.Set;
 
 public class ModelManagerTest {
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+    private static final String LAYOUT_PATTERNS = "layoutPatterns";
 
-    @Test
-    public void testConversionPatternLayput() {
+    @DataProvider(name = LAYOUT_PATTERNS) //
+    public Object[][] dataSetForLayoutPatterns() {
+        return new Object[][] {
+                {"%d{UNIX_MILLIS} %X{httpUri} %X{httpStatus} %message%n"},
+                {"%date{DEFAULT} [%level] [%thread] [%class{36}] [%thread] %message%n"}
+        };
+    }
 
+    @Test(dataProvider = LAYOUT_PATTERNS)
+    public void testConversionPatternLayput(final String pattern) {
         ModelManager modelManager = new ModelManager();
-        Map<String, String> hmap =  modelManager.convertLayoutPatternToMap("%date{DEFAULT} [%level] [%thread] [%class{36}] [%thread] %message%n");
+        Map<String, String> hmap =  modelManager.convertLayoutPatternToMap(pattern);
 
         Set keys = hmap.keySet();
         Iterator iterator = keys.iterator();
@@ -22,8 +34,7 @@ public class ModelManagerTest {
             String key = (String) iterator.next();
             String value = hmap.get(key);
 
-            System.out.println("LogEvent Key : " + key);
-            System.out.println("LogEvent Value : " + value);
+           LOGGER.info("Extracted Key and Value: {} -> {}", key , value);
         }
 
 
