0135374f682d40cc7e872f34c315f66620229617
SrideviJava <jsridevi@gmail.com>
2018-11-14 22:08:40 +0100

    Added POJO to create JSON

--- src/main/java/net/technolords/library/log4j2/model/LogEventAsJson.java
+++ src/main/java/net/technolords/library/log4j2/model/LogEventAsJson.java
@@ -1,5 +1,13 @@
 package net.technolords.library.log4j2.model;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+import java.util.Map;
+import java.util.logging.Level;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class LogEventAsJson {
     // 'basics'
     // context-data aka mdc (key, value pairs)
@@ -19,4 +27,126 @@ public class LogEventAsJson {
     2018-11-05 20:26:32,802 main INFO StackTraceElement: net.technolords.proto.log4j2.Sample.initAndRun(Sample.java:15)
     2018-11-05 20:26:32,802 main INFO Thowable: {}
      */
+
+    private Level logLevel;
+    private long timestamp;
+    private int threadId;
+    private String logMessage;
+    private String formattedMessage;
+    private String marker;
+    private String className;
+    private Map<String, String> threadContextMap;
+    private String threadContextStack;
+    private String threadContextStackTrace;
+    private String exception;
+
+    @JsonGetter("logLevel")
+    public Level getLogLevel() {
+        return logLevel;
+    }
+
+    @JsonSetter("logLevel")
+    public void setLogLevel(Level logLevel) {
+        this.logLevel = logLevel;
+    }
+
+    @JsonGetter("timestamp")
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonSetter("timestamp")
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @JsonGetter("threadId")
+    public int getThreadId() {
+        return threadId;
+    }
+
+    @JsonSetter("threadId")
+    public void setThreadId(int threadId) {
+        this.threadId = threadId;
+    }
+
+    @JsonGetter("logMessage")
+    public String getLogMessage() {
+        return logMessage;
+    }
+
+    @JsonSetter("logMessage")
+    public void setLogMessage(String logMessage) {
+        this.logMessage = logMessage;
+    }
+
+    @JsonGetter("formattedMessage")
+    public String getFormattedMessage() {
+        return formattedMessage;
+    }
+
+    @JsonSetter("formattedMessage")
+    public void setFormattedMessage(String formattedMessage) {
+        this.formattedMessage = formattedMessage;
+    }
+
+    @JsonGetter("marker")
+    public String getMarker() {
+        return marker;
+    }
+
+    @JsonSetter("marker")
+    public void setMarker(String marker) {
+        this.marker = marker;
+    }
+
+    @JsonGetter("className")
+    public String getClassName() {
+        return className;
+    }
+
+    @JsonSetter("className")
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    @JsonGetter("threadContextMap")
+    public Map<String, String> getThreadContextMap() {
+        return threadContextMap;
+    }
+
+    @JsonSetter("threadContextMap")
+    public void setThreadContextMap(Map<String, String> threadContextMap) {
+        this.threadContextMap = threadContextMap;
+    }
+
+    @JsonGetter("threadContextStack")
+    public String getThreadContextStack() {
+        return threadContextStack;
+    }
+
+    @JsonSetter("threadContextStack")
+    public void setThreadContextStack(String threadContextStack) {
+        this.threadContextStack = threadContextStack;
+    }
+
+    @JsonGetter("threadContextStackTrace")
+    public String getThreadContextStackTrace() {
+        return threadContextStackTrace;
+    }
+
+    @JsonSetter("threadContextStackTrace")
+    public void setThreadContextStackTrace(String threadContextStackTrace) {
+        this.threadContextStackTrace = threadContextStackTrace;
+    }
+
+    @JsonGetter("exception")
+    public String getException() {
+        return exception;
+    }
+
+    @JsonSetter("exception")
+    public void setException(String exception) {
+        this.exception = exception;
+    }
 }
--- src/test/main/java/net.technolords.library.log4j2/ModelManagerTest.java
+++ src/test/main/java/net.technolords.library.log4j2/ModelManagerTest.java
@@ -1,42 +1,42 @@
-package net.technolords.library.log4j2;
-
-import net.technolords.library.log4j2.model.ModelManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-public class ModelManagerTest {
-    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    private static final String LAYOUT_PATTERNS = "layoutPatterns";
-
-    @DataProvider(name = LAYOUT_PATTERNS) //
-    public Object[][] dataSetForLayoutPatterns() {
-        return new Object[][] {
-                {"%d{UNIX_MILLIS} %X{httpUri} %X{httpStatus} %message%n"},
-                {"%date{DEFAULT} [%level] [%thread] [%class{36}] [%thread] %message%n"}
-        };
-    }
-
-    @Test(dataProvider = LAYOUT_PATTERNS)
-    public void testConversionPatternLayput(final String pattern) {
-        ModelManager modelManager = new ModelManager();
-        Map<String, String> hmap =  modelManager.convertLayoutPatternToMap(pattern);
-
-        Set keys = hmap.keySet();
-        Iterator iterator = keys.iterator();
-
-        while (iterator.hasNext()) {
-            String key = (String) iterator.next();
-            String value = hmap.get(key);
-
-           LOGGER.info("Extracted Key and Value: {} -> {}", key , value);
-        }
-
-
-    }
-}
+//package net.technolords.library.log4j2;
+//
+//import net.technolords.library.log4j2.model.ModelManager;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.testng.annotations.DataProvider;
+//import org.testng.annotations.Test;
+//
+//import java.util.Iterator;
+//import java.util.Map;
+//import java.util.Set;
+//
+//public class ModelManagerTest {
+//    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+//    private static final String LAYOUT_PATTERNS = "layoutPatterns";
+//
+//    @DataProvider(name = LAYOUT_PATTERNS) //
+//    public Object[][] dataSetForLayoutPatterns() {
+//        return new Object[][] {
+//                {"%d{UNIX_MILLIS} %X{httpUri} %X{httpStatus} %message%n"},
+//                {"%date{DEFAULT} [%level] [%thread] [%class{36}] [%thread] %message%n"}
+//        };
+//    }
+//
+//    @Test(dataProvider = LAYOUT_PATTERNS)
+//    public void testConversionPatternLayput(final String pattern) {
+//        ModelManager modelManager = new ModelManager();
+////        Map<String, String> hmap =  modelManager(pattern);
+//
+//        Set keys = hmap.keySet();
+//        Iterator iterator = keys.iterator();
+//
+//        while (iterator.hasNext()) {
+//            String key = (String) iterator.next();
+//            String value = hmap.get(key);
+//
+//           LOGGER.info("Extracted Key and Value: {} -> {}", key , value);
+//        }
+//
+//
+//    }
+//}
